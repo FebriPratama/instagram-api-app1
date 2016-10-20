3c7ae55705eb66e740e6ffcfe39bf755b284cda7
FebriPratama <febri.pr@students.amikom.ac.id>
2016-10-20 17:27:18 +0700

    reinit

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,7 @@ var Config = {
   // Contents of this file will be send to the client
   "domain": '127.0.0.1',
 
-  "serverip": '127.0.0.1',
+  "serverip": '0.0.0.0',
   "serverport": '3000',
   
   "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
